e176da664a59a4e1f668b384324d25c65b80cabd
blakej2432 <jmchan2432@gmail.com>
2022-06-30 21:17:03 +0900

    Create MIT_Algorithm.py

---
+++ MIT_Algorithm.py
@@ -0,0 +1,39 @@
+MIT - 파이썬을 이용한 알고리즘 이해 강의
+
+알고리즘
+
+https://courses.csail.mit.edu/6.006
+
+Course Overview
+
+"큰 입력을 받는 문제를 처리하기 위한 효율적인 방법"
+
+큰 입력 예시 human genes, USA highway system
+
+Efficient procedures for solving large scale problems
+Scalability(확장성)
+Clssic data structures & classical algorithms
+real implementations in Python
+
+Content
+8 modiles:
+    1) Algorithmic thinking: peak finding
+    2) Sorting & trees : Event simulation
+    3) Hashing: genome comparison
+    4) Numerics: RSA encryption
+    5) Graphs: Rubik's cube
+    6) Shortest paths: Caltech -> MIT
+    7) Dynamic programming: Image comparison
+    8) Advanced topics
+
+
+Peak finding
+
+
+
+
+
+
+
+
+
