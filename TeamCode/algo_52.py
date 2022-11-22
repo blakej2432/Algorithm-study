04b82b1b3d24f1d46a10a8a011f435f0840d6ca7
정명찬/Blake Jeong <jmchan2432@gmail.com>
2022-11-22 15:41:39 +0900

    bfs
    백준 실버

---
+++ TeamCode/algo_52.py
@@ -0,0 +1,38 @@
+# 데스나이트
+
+import sys
+from collections import deque
+
+
+N = int(sys.stdin.readline().rstrip())
+
+a, b, c, d = map(int, sys.stdin.readline().split())
+
+graph = []
+for i in range(N):
+    graph.append([-1]*N)
+
+dx = [-2, -2, 0, 0, 2, 2]
+dy = [-1, 1, -2, 2, -1, 1]
+
+def bfs(x, y):
+    queue = deque()
+    queue.append((x,y))
+    graph[x][y] = 0
+
+    while queue:
+        x, y = queue.popleft()
+
+        for i in range(6):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if 0 <= nx < N and 0 <= ny < N and graph[nx][ny] == -1:
+                graph[nx][ny] = graph[x][y] + 1
+                queue.append((nx, ny))
+
+    return graph[c][d]
+
+print(bfs(a, b))
+
+
