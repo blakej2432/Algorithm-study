32f948edcf683975f078aaf368f99cbe8f91b7e9
정명찬/Blake Jeong <jmchan2432@gmail.com>
2022-12-08 23:09:13 +0900

    덱
    백준 실버

---
+++ TeamCode/algo_73.py
@@ -0,0 +1,59 @@
+# 덱
+
+import sys
+from collections import deque
+
+
+N = int(sys.stdin.readline().rstrip())
+
+# 리스트 비어있는지 확인하기 -> if not queue, 무조건 len()으로 하려하지말자
+queue = deque()
+def Q(x):
+	if x[0] == 'empty':
+		if not queue:
+			print(1)
+		else:
+			print(0)
+	if x[0] == 'size':
+		print(len(queue))
+	if x[0] == 'push_front':
+		queue.appendleft(x[1])
+	if x[0] == 'push_back':
+		queue.append(x[1])
+	if x[0] == 'pop_front':
+		if not queue:
+			print(-1)
+		else:
+			print(queue.popleft())
+	if x[0] == 'pop_back':
+		if not queue:
+			print(-1)
+		else:
+			print(queue.pop())
+	if x[0] == 'front':
+		if not queue:
+			print(-1)
+		else:
+			print(queue[0])
+	if x[0] == 'back':
+		if not queue:
+			print(-1)
+		else:
+			print(queue[-1])
+
+
+
+for _ in range(N):
+	Q(sys.stdin.readline().split())
+
+
+
+
+
+
+
+
+
+
+
+
